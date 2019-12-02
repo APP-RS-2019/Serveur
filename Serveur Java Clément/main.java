a8e39d6e24cf6139487499057a49187b792b44d0
APP-RS-2019 <47772605+APP-RS-2019@users.noreply.github.com>
2019-12-02 17:18:02 +0100

    Create main.java

---
+++ Serveur Java Clément/main.java
@@ -0,0 +1,15 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		try {
+			ClientSocket test=new ClientSocket("193.48.125.71",80,"appliTest");
+			test.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
