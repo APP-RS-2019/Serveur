694d392b1f9b3c3730aeacdf318cbe5888e28060
APP-RS-2019 <47772605+APP-RS-2019@users.noreply.github.com>
2019-12-11 09:27:54 +0100

    Update main.java

--- Serveur Java Clément/main.java
+++ Serveur Java Clément/main.java
@@ -4,6 +4,7 @@ public class main {
 	public static void main(String[] args) {
 		try {
 			ClientSocket test=new ClientSocket("193.48.125.71",80,"appliTest");
+			test.send("raspberry","salut");
 			test.close();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
